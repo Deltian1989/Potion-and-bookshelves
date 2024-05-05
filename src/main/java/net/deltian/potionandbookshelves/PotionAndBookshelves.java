b13df17815a53273d0dca7b5c84371a8d3318e5e
Dorian Jarosz <deltian1989@gmail.com>
2024-05-05 16:44:47 +0200

    Task with project setup complete

---
+++ src/main/java/net/deltian/potionandbookshelves/PotionAndBookshelves.java
@@ -0,0 +1,60 @@
+package net.deltian.potionandbookshelves;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+import static net.deltian.potionandbookshelves.PotionAndBookshelves.MOD_ID;
+
+@Mod(MOD_ID)
+public class PotionAndBookshelves
+{
+    public static final String MOD_ID = "potionandbookshelves";
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public PotionAndBookshelves()
+    {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+    }
+
+    private void enqueueIMC(final InterModEnqueueEvent event)
+    {
+    }
+
+    private void processIMC(final InterModProcessEvent event)
+    {
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+    }
+
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents
+    {
+        @SubscribeEvent
+        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent)
+        {
+
+        }
+    }
+}
